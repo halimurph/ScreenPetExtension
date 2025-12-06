6476e061c1ed573799d4b27eea69b4de86e1b72d
halimurph <167351997+halimurph@users.noreply.github.com>
2025-12-06 10:00:38 -0600

    Update ScreenPetGame.js

--- ScreenPetGame.js
+++ ScreenPetGame.js
@@ -454,9 +454,9 @@ class ScreenPetGame {
             }
 
             if (this.movingLeft) {
-                p.image(this.babyDuckImages[this.currentFrame], this.babyDuckX + 5, this.babyDuckY - 10);
+                p.image(this.babyDuckImages[this.currentFrame], this.babyDuckX + 5, this.babyDuckY - 10,10,10);
             } else {
-                p.image(this.babyDuckImages2[this.currentFrame], this.babyDuckX + 5, this.babyDuckY - 10);
+                p.image(this.babyDuckImages2[this.currentFrame], this.babyDuckX + 5, this.babyDuckY - 10),10,10;
             }
         }
     }
